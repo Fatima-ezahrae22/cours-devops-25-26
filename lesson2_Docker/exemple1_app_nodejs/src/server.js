5c64f57d1fdf91bd958fef5f84fddc756fdc9ef7
Fatima-ezahrae mekki <fatimekki61@gmail.com>
2025-12-07 23:49:16 +0100

    Update server.js

--- lesson2_Docker/exemple1_app_nodejs/src/server.js
+++ lesson2_Docker/exemple1_app_nodejs/src/server.js
@@ -4,10 +4,10 @@ const app = express();
 app.get('/', (req, res) => {
   res.send(`
     <h1>Welcome to my awesome app!</h1>
+     <h1>Welcome </h1>
     <img src="https://i.pinimg.com/originals/cb/22/60/cb22603604e36d1caab7cc4e729506c9.gif" alt="Beautiful Landscape" style="max-width:400px;">
   `);
 });
-<h1> fatima ezahrae mekki</h1>
 app.listen(8088, function () {
   console.log("app listening on port 8088, go to http://localhost:8088");
 });
